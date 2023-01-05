ee2badbebf363b2fa7acc3456ca036f5c93d46d1
Dima Pristupa <dima.pristupa@pm.me>
2023-01-05 19:43:02 +0200

    🚧 2.6. Dealing Damage - Indexing what is where: finish

--- src/map_indexing_system.rs
+++ src/map_indexing_system.rs
@@ -9,17 +9,26 @@ impl<'a> System<'a> for MapIndexingSystem {
         WriteExpect<'a, Map>,
         ReadStorage<'a, Position>,
         ReadStorage<'a, BlocksTile>,
+        Entities<'a>,
     );
 
     fn run(&mut self, data: Self::SystemData) {
-        let (mut map, position, blockers) = data;
+        let (mut map, position, blockers, entities) = data;
 
         map.populate_blocked();
+        map.clear_content_index();
 
-        for (position, _blocks) in (&position, &blockers).join() {
+        for (entity, position) in (&entities, &position).join() {
             let idx = map.xy_idx(position.x, position.y);
 
-            map.blocked[idx] = true;
+            // If they block, update the blocking list
+            if blockers.get(entity).is_some() {
+                map.blocked[idx] = true;
+            }
+
+            // Push the entity to the appropriate index slot. It's a Copy
+            // type, so we don't need to clone it (we want to avoid moving it out of the ECS!)
+            map.tile_content[idx].push(entity);
         }
     }
 }
