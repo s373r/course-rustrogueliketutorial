70f1f574dda1f5c71b8aceaaf27698b0f53f8031
Dima Pristupa <dima.pristupa@pm.me>
2023-01-05 14:02:07 +0200

    🚧 2.3. A More Interesting Map: Joining the rooms together: finish

--- src/map.rs
+++ src/map.rs
@@ -66,6 +66,20 @@ pub fn new_map_rooms_and_corridors() -> Vec<TileType> {
 
         if !has_intersect_other_rooms {
             apply_room_to_map(&new_room, &mut map);
+
+            if !rooms.is_empty() {
+                let (new_x, new_y) = new_room.center();
+                let (prev_x, prev_y) = rooms[rooms.len() - 1].center();
+
+                if rng.range(0, 2) == 1 {
+                    apply_horizontal_tunnel(&mut map, prev_x, new_x, prev_y);
+                    apply_vertical_tunnel(&mut map, prev_y, new_y, new_x);
+                } else {
+                    apply_vertical_tunnel(&mut map, prev_y, new_y, prev_x);
+                    apply_horizontal_tunnel(&mut map, prev_x, new_x, new_y);
+                }
+            }
+
             rooms.push(new_room);
         }
     }
