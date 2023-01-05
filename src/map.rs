105e64bd0bd995c1b3ce0e8a97722faeb4d59e58
Dima Pristupa <dima.pristupa@pm.me>
2023-01-05 14:06:34 +0200

    🚧 2.3. A More Interesting Map: Placing the player: finish

--- src/map.rs
+++ src/map.rs
@@ -44,7 +44,7 @@ pub fn new_map_test() -> Vec<TileType> {
     map
 }
 
-pub fn new_map_rooms_and_corridors() -> Vec<TileType> {
+pub fn new_map_rooms_and_corridors() -> (Vec<Rect>, Vec<TileType>) {
     let mut map = vec![TileType::Wall; 80 * 50];
 
     let mut rooms: Vec<Rect> = Vec::new();
@@ -84,7 +84,7 @@ pub fn new_map_rooms_and_corridors() -> Vec<TileType> {
         }
     }
 
-    map
+    (rooms, map)
 }
 
 fn apply_room_to_map(room: &Rect, map: &mut [TileType]) {
