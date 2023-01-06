3d9bec2f92e69bf056dfa7dd83e4bacc59605faf
Dima Pristupa <dima.pristupa@pm.me>
2023-01-06 16:02:55 +0200

    ✨ 2.7. User Interface: finish

--- src/main.rs
+++ src/main.rs
@@ -108,9 +108,12 @@ impl GameState for State {
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
 
-    let context = RltkBuilder::simple80x50()
+    let mut context = RltkBuilder::simple80x50()
         .with_title("Roguelike Tutorial")
         .build()?;
+
+    context.with_post_scanlines(true);
+
     let mut gs = State { ecs: World::new() };
 
     gs.ecs.register::<Position>();
