aa547c7ef95bceb1518be43c1693a4c625595269
Dima Pristupa <dima.pristupa@pm.me>
2023-01-12 16:20:32 +0200

    🚧 3.2. Bloodstains - Rendering the blood: finish

--- src/map.rs
+++ src/map.rs
@@ -2,6 +2,7 @@ use rltk::{Algorithm2D, BaseMap, Point, RandomNumberGenerator, Rltk, RGB};
 use serde::{Deserialize, Serialize};
 use specs::prelude::*;
 use std::cmp::{max, min};
+use std::collections::HashSet;
 
 use crate::rect::Rect;
 
@@ -22,6 +23,8 @@ pub struct Map {
     pub visible_tiles: Vec<bool>,
     pub blocked: Vec<bool>,
     pub depth: i32,
+    pub bloodstains: HashSet<usize>,
+
     #[serde(skip_serializing)]
     #[serde(skip_deserializing)]
     pub tile_content: Vec<Vec<Entity>>,
@@ -74,6 +77,7 @@ impl Map {
             visible_tiles: vec![false; Self::LENGTH],
             blocked: vec![false; Self::LENGTH],
             depth: new_depth,
+            bloodstains: HashSet::new(),
             tile_content: vec![Vec::new(); Self::LENGTH],
         };
 
@@ -216,11 +220,17 @@ pub fn draw_map(ecs: &World, ctx: &mut Rltk) {
                 TileType::DownStairs => (rltk::to_cp437('>'), RGB::from_f32(0., 1.0, 1.0)),
             };
 
+            let mut bg = RGB::from_f32(0., 0., 0.);
+
+            if map.bloodstains.contains(&idx) {
+                bg = RGB::from_f32(0.75, 0., 0.);
+            }
+
             if !map.visible_tiles[idx] {
                 fg = fg.to_greyscale()
             }
 
-            ctx.set(x, y, fg, RGB::from_f32(0., 0., 0.), glyph);
+            ctx.set(x, y, fg, bg, glyph);
         }
 
         // Move the coordinates
