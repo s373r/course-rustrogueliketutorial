ccf45f04ee2162906d928a814e1963c995ded98f
Dima Pristupa <dima.pristupa@pm.me>
2023-01-12 16:41:08 +0200

    ♻️ RenderOrder: remove extra explicit items value initialization

--- src/render_order.rs
+++ src/render_order.rs
@@ -2,9 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Ord, PartialOrd, Eq, PartialEq, Deserialize, Serialize, Copy, Clone)]
 pub enum RenderOrder {
-    Player = 0,
-    // TODO(DP): check with the internet:
-    //           do we need to specify values for all rest items?
-    Monster = 1,
-    Item = 2,
+    Player,
+    Monster,
+    Item,
 }
