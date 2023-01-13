ca88a9c26d036ec70aaaf23ea24bd599906f017d
Dima Pristupa <dima.pristupa@pm.me>
2023-01-13 19:44:33 +0200

    ♻️ VisibilitySystem::run(): improve readability

--- src/visibility_system.rs
+++ src/visibility_system.rs
@@ -35,49 +35,47 @@ impl<'a> System<'a> for VisibilitySystem {
 
         // TODO(DP): use early ~~returns~~ continues
         for (ent, viewshed, pos) in (&entities, &mut viewshed, &pos).join() {
-            if viewshed.dirty {
-                viewshed.dirty = false;
-                // TODO(DP): remove
-                viewshed.visible_tiles.clear();
-                viewshed.visible_tiles =
-                    field_of_view(Point::new(pos.x, pos.y), viewshed.range, &*map);
-                viewshed
-                    .visible_tiles
-                    .retain(|p| p.x >= 0 && p.x < map.width && p.y >= 0 && p.y < map.height);
+            if !viewshed.dirty {
+                continue;
+            }
 
-                // If this is the player, reveal what they can see
-                if player.get(ent).is_some() {
-                    // TODO(DP): use fill()
-                    for visible_tile in map.visible_tiles.iter_mut() {
-                        *visible_tile = false
+            viewshed.dirty = false;
+            viewshed.visible_tiles = field_of_view(Point::new(pos.x, pos.y), viewshed.range, &*map);
+            viewshed
+                .visible_tiles
+                .retain(|p| p.x >= 0 && p.x < map.width && p.y >= 0 && p.y < map.height);
+
+            // If this is the player, reveal what they can see
+            if player.get(ent).is_none() {
+                continue;
+            }
+
+            map.visible_tiles.fill(false);
+
+            for vis in viewshed.visible_tiles.iter() {
+                let idx = map.xy_idx(vis.x, vis.y);
+                map.revealed_tiles[idx] = true;
+                map.visible_tiles[idx] = true;
+
+                // Chance to reveal hidden things
+                for tile_entity in map.tile_content[idx].iter() {
+                    let maybe_hidden = hidden.get(*tile_entity);
+
+                    if maybe_hidden.is_none() {
+                        continue;
                     }
 
-                    for vis in viewshed.visible_tiles.iter() {
-                        let idx = map.xy_idx(vis.x, vis.y);
-                        map.revealed_tiles[idx] = true;
-                        map.visible_tiles[idx] = true;
-
-                        // Chance to reveal hidden things
-                        for tile_entity in map.tile_content[idx].iter() {
-                            let maybe_hidden = hidden.get(*tile_entity);
-
-                            if maybe_hidden.is_none() {
-                                continue;
-                            }
-
-                            if rng.roll_dice(1, 24) != 1 {
-                                continue;
-                            }
-
-                            if let Some(name) = names.get(*tile_entity) {
-                                game_log
-                                    .entries
-                                    .push(format!("You spotted a {}.", &name.name));
-                            }
-
-                            hidden.remove(*tile_entity);
-                        }
+                    if rng.roll_dice(1, 24) != 1 {
+                        continue;
                     }
+
+                    if let Some(name) = names.get(*tile_entity) {
+                        game_log
+                            .entries
+                            .push(format!("You spotted a {}.", &name.name));
+                    }
+
+                    hidden.remove(*tile_entity);
                 }
             }
         }
