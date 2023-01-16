5f3014b258003876166694b0991ef2fd4121223e
Dima Pristupa <dima.pristupa@pm.me>
2023-01-16 19:42:00 +0200

    🚧 4.5. Cellular Automata Maps - Putting together the basic map: finish

--- src/map_builders/cellular_automata.rs
+++ src/map_builders/cellular_automata.rs
@@ -2,7 +2,7 @@ use rltk::RandomNumberGenerator;
 use specs::prelude::*;
 
 use crate::components::Position;
-use crate::map::Map;
+use crate::map::{Map, TileType};
 use crate::map_builders::MapBuilder;
 use crate::SHOW_MAPGEN_VISUALIZER;
 
@@ -17,7 +17,7 @@ pub struct CellularAutomataBuilder {
 
 impl MapBuilder for CellularAutomataBuilder {
     fn build_map(&mut self) {
-        //self.build(); - we should write this
+        self.build();
     }
 
     fn spawn_entities(&self, ecs: &mut World) {
@@ -56,6 +56,73 @@ impl CellularAutomataBuilder {
             starting_position: Position { x: 0, y: 0 },
             depth: new_depth,
             history: Vec::new(),
+        }
+    }
+
+    fn build(&mut self) {
+        let mut rng = RandomNumberGenerator::new();
+
+        // First we completely randomize the map, setting 55% of it to be floor.
+        for y in 1..self.map.height - 1 {
+            for x in 1..self.map.width - 1 {
+                let roll = rng.roll_dice(1, 100);
+                let idx = self.map.xy_idx(x, y);
+
+                if roll > 55 {
+                    self.map.tiles[idx] = TileType::Floor
+                } else {
+                    self.map.tiles[idx] = TileType::Wall
+                }
+            }
+        }
+
+        self.take_snapshot();
+
+        // Now we iteratively apply cellular automata rules
+        for _ in 0..15 {
+            let mut new_tiles = self.map.tiles.clone();
+
+            for y in 1..self.map.height - 1 {
+                for x in 1..self.map.width - 1 {
+                    let idx = self.map.xy_idx(x, y);
+                    let mut neighbors = 0;
+
+                    if self.map.tiles[idx - 1] == TileType::Wall {
+                        neighbors += 1;
+                    }
+                    if self.map.tiles[idx + 1] == TileType::Wall {
+                        neighbors += 1;
+                    }
+                    if self.map.tiles[idx - self.map.width as usize] == TileType::Wall {
+                        neighbors += 1;
+                    }
+                    if self.map.tiles[idx + self.map.width as usize] == TileType::Wall {
+                        neighbors += 1;
+                    }
+                    if self.map.tiles[idx - (self.map.width as usize - 1)] == TileType::Wall {
+                        neighbors += 1;
+                    }
+                    if self.map.tiles[idx - (self.map.width as usize + 1)] == TileType::Wall {
+                        neighbors += 1;
+                    }
+                    if self.map.tiles[idx + (self.map.width as usize - 1)] == TileType::Wall {
+                        neighbors += 1;
+                    }
+                    if self.map.tiles[idx + (self.map.width as usize + 1)] == TileType::Wall {
+                        neighbors += 1;
+                    }
+
+                    if neighbors > 4 || neighbors == 0 {
+                        new_tiles[idx] = TileType::Wall;
+                    } else {
+                        new_tiles[idx] = TileType::Floor;
+                    }
+                }
+            }
+
+            self.map.tiles = new_tiles;
+
+            self.take_snapshot();
         }
     }
 }
