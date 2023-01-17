2d9044335afd479c6da8c32b58e764da6c6e5350
Dima Pristupa <dima.pristupa@pm.me>
2023-01-17 19:37:36 +0200

    🚧 4.5. Cellular Automata Maps - Placing an exit - and culling unreachable areas: finish

--- src/map_builders/cellular_automata.rs
+++ src/map_builders/cellular_automata.rs
@@ -142,5 +142,43 @@ impl CellularAutomataBuilder {
                 .map
                 .xy_idx(self.starting_position.x, self.starting_position.y);
         }
+
+        // Find all tiles we can reach from the starting point
+        let map_starts = vec![start_idx];
+        let dijkstra_map = rltk::DijkstraMap::new(
+            self.map.width,
+            self.map.height,
+            &map_starts,
+            &self.map,
+            200.0,
+        );
+        let mut exit_tile = (0, 0.0f32);
+
+        for (i, tile) in self.map.tiles.iter_mut().enumerate() {
+            if *tile != TileType::Floor {
+                continue;
+            }
+
+            let distance_to_start = dijkstra_map.map[i];
+
+            // We can't get to this tile - so we'll make it a wall
+            if distance_to_start == f32::MAX {
+                *tile = TileType::Wall;
+
+                continue;
+            }
+
+            // If it is further away than our current exit candidate, move the exit
+            if distance_to_start > exit_tile.1 {
+                exit_tile.0 = i;
+                exit_tile.1 = distance_to_start;
+            }
+        }
+
+        self.take_snapshot();
+
+        self.map.tiles[exit_tile.0] = TileType::DownStairs;
+
+        self.take_snapshot();
     }
 }
