3aedc61fa47e900cb4500cc88356b0e3ed10263b
Dima Pristupa <dima.pristupa@pm.me>
2023-01-19 19:40:19 +0200

    🚧 4.6. Drunkard's Walk Maps - Don't Repeat Yourself (The DRY principle): finish

--- src/map_builders/cellular_automata.rs
+++ src/map_builders/cellular_automata.rs
@@ -4,6 +4,9 @@ use std::collections::HashMap;
 
 use crate::components::Position;
 use crate::map::{Map, TileType};
+use crate::map_builders::common::{
+    generate_voronoi_spawn_regions, remove_unreachable_areas_returning_most_distant,
+};
 use crate::map_builders::MapBuilder;
 use crate::{spawner, SHOW_MAPGEN_VISUALIZER};
 
@@ -150,68 +153,16 @@ impl CellularAutomataBuilder {
         }
 
         // Find all tiles we can reach from the starting point
-        let map_starts = vec![start_idx];
-        let dijkstra_map = rltk::DijkstraMap::new(
-            self.map.width,
-            self.map.height,
-            &map_starts,
-            &self.map,
-            200.0,
-        );
-        let mut exit_tile = (0, 0.0f32);
-
-        for (i, tile) in self.map.tiles.iter_mut().enumerate() {
-            if *tile != TileType::Floor {
-                continue;
-            }
-
-            let distance_to_start = dijkstra_map.map[i];
-
-            // TODO(DP): this does not work -- unreachable cells have no f32::MAX value
-            // We can't get to this tile - so we'll make it a wall
-            if distance_to_start == f32::MAX {
-                *tile = TileType::Wall;
-
-                continue;
-            }
-
-            // If it is further away than our current exit candidate, move the exit
-            if distance_to_start > exit_tile.1 {
-                exit_tile.0 = i;
-                exit_tile.1 = distance_to_start;
-            }
-        }
+        let exit_tile = remove_unreachable_areas_returning_most_distant(&mut self.map, start_idx);
 
         self.take_snapshot();
 
-        self.map.tiles[exit_tile.0] = TileType::DownStairs;
+        // Place the stairs
+        self.map.tiles[exit_tile] = TileType::DownStairs;
 
         self.take_snapshot();
 
         // Now we build a noise map for use in spawning entities later
-        let mut noise = rltk::FastNoise::seeded(rng.roll_dice(1, 65536) as u64);
-
-        noise.set_noise_type(rltk::NoiseType::Cellular);
-        noise.set_frequency(0.08);
-        noise.set_cellular_distance_function(rltk::CellularDistanceFunction::Manhattan);
-
-        for y in 1..self.map.height - 1 {
-            for x in 1..self.map.width - 1 {
-                let idx = self.map.xy_idx(x, y);
-
-                if self.map.tiles[idx] != TileType::Floor {
-                    continue;
-                }
-
-                let cell_value_f = noise.get_noise(x as f32, y as f32) * 10240.0;
-                let cell_value = cell_value_f as i32;
-
-                if self.noise_areas.contains_key(&cell_value) {
-                    self.noise_areas.get_mut(&cell_value).unwrap().push(idx);
-                } else {
-                    self.noise_areas.insert(cell_value, vec![idx]);
-                }
-            }
-        }
+        self.noise_areas = generate_voronoi_spawn_regions(&self.map, &mut rng);
     }
 }
