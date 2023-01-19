f16be447f04f7b38acceded0acdd21c0fd283e2a
Dima Pristupa <dima.pristupa@pm.me>
2023-01-19 20:20:17 +0200

    🚧 4.7. Mazes and Labyrinths - Scaffolding: finish

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -3,6 +3,7 @@ mod bsp_interior;
 mod cellular_automata;
 mod common;
 mod drunkard;
+mod maze;
 mod simple_map;
 
 use rltk::RandomNumberGenerator;
@@ -14,6 +15,7 @@ use crate::map_builders::bsp_dungeon::BspDungeonBuilder;
 use crate::map_builders::bsp_interior::BspInteriorBuilder;
 use crate::map_builders::cellular_automata::CellularAutomataBuilder;
 use crate::map_builders::drunkard::DrunkardsWalkBuilder;
+use crate::map_builders::maze::MazeBuilder;
 use crate::map_builders::simple_map::SimpleMapBuilder;
 
 pub trait MapBuilder {
@@ -26,16 +28,19 @@ pub trait MapBuilder {
 }
 
 pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
-    let mut rng = RandomNumberGenerator::new();
-    let builder = rng.roll_dice(1, 7);
+    // TODO(DP): uncomment after tests
+    // let mut rng = RandomNumberGenerator::new();
+    // let builder = rng.roll_dice(1, 7);
+    //
+    // match builder {
+    //     1 => Box::new(BspDungeonBuilder::new(new_depth)),
+    //     2 => Box::new(BspInteriorBuilder::new(new_depth)),
+    //     3 => Box::new(CellularAutomataBuilder::new(new_depth)),
+    //     4 => Box::new(DrunkardsWalkBuilder::open_area(new_depth)),
+    //     5 => Box::new(DrunkardsWalkBuilder::open_halls(new_depth)),
+    //     6 => Box::new(DrunkardsWalkBuilder::winding_passages(new_depth)),
+    //     _ => Box::new(SimpleMapBuilder::new(new_depth)),
+    // }
 
-    match builder {
-        1 => Box::new(BspDungeonBuilder::new(new_depth)),
-        2 => Box::new(BspInteriorBuilder::new(new_depth)),
-        3 => Box::new(CellularAutomataBuilder::new(new_depth)),
-        4 => Box::new(DrunkardsWalkBuilder::open_area(new_depth)),
-        5 => Box::new(DrunkardsWalkBuilder::open_halls(new_depth)),
-        6 => Box::new(DrunkardsWalkBuilder::winding_passages(new_depth)),
-        _ => Box::new(SimpleMapBuilder::new(new_depth)),
-    }
+    Box::new(MazeBuilder::new(new_depth))
 }
