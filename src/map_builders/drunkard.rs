649fcb292dabdfbe9e4d14c91af64f8341b8ad31
Dima Pristupa <dima.pristupa@pm.me>
2023-01-19 19:33:37 +0200

    🚧 4.6. Drunkard's Walk Maps - Initial scaffolding: start

---
+++ src/map_builders/drunkard.rs
@@ -0,0 +1,215 @@
+use rltk::RandomNumberGenerator;
+use specs::prelude::*;
+use std::collections::HashMap;
+
+use crate::components::Position;
+use crate::map::{Map, TileType};
+use crate::map_builders::MapBuilder;
+use crate::{spawner, SHOW_MAPGEN_VISUALIZER};
+
+pub struct DrunkardsWalkBuilder {
+    map: Map,
+    starting_position: Position,
+    depth: i32,
+    history: Vec<Map>,
+    noise_areas: HashMap<i32, Vec<usize>>,
+}
+
+impl MapBuilder for DrunkardsWalkBuilder {
+    fn build_map(&mut self) {
+        self.build();
+    }
+
+    fn spawn_entities(&self, ecs: &mut World) {
+        for area in self.noise_areas.iter() {
+            spawner::spawn_region(ecs, area.1, self.depth);
+        }
+    }
+
+    fn get_map(&self) -> Map {
+        self.map.clone()
+    }
+
+    fn get_starting_position(&self) -> Position {
+        self.starting_position.clone()
+    }
+
+    fn get_snapshot_history(&self) -> Vec<Map> {
+        self.history.clone()
+    }
+
+    fn take_snapshot(&mut self) {
+        if !SHOW_MAPGEN_VISUALIZER {
+            return;
+        }
+
+        let mut snapshot = self.map.clone();
+
+        snapshot.revealed_tiles.fill(true);
+
+        self.history.push(snapshot);
+    }
+}
+
+impl DrunkardsWalkBuilder {
+    pub fn new(new_depth: i32) -> DrunkardsWalkBuilder {
+        DrunkardsWalkBuilder {
+            map: Map::new(new_depth),
+            starting_position: Position { x: 0, y: 0 },
+            depth: new_depth,
+            history: Vec::new(),
+            noise_areas: HashMap::new(),
+        }
+    }
+
+    #[allow(clippy::map_entry)]
+    fn build(&mut self) {
+        let mut rng = RandomNumberGenerator::new();
+
+        // First we completely randomize the map, setting 55% of it to be floor.
+        for y in 1..self.map.height - 1 {
+            for x in 1..self.map.width - 1 {
+                let roll = rng.roll_dice(1, 100);
+                let idx = self.map.xy_idx(x, y);
+
+                if roll > 55 {
+                    self.map.tiles[idx] = TileType::Floor
+                } else {
+                    self.map.tiles[idx] = TileType::Wall
+                }
+            }
+        }
+
+        self.take_snapshot();
+
+        // Now we iteratively apply cellular automata rules
+        for _ in 0..15 {
+            let mut new_tiles = self.map.tiles.clone();
+
+            for y in 1..self.map.height - 1 {
+                for x in 1..self.map.width - 1 {
+                    let idx = self.map.xy_idx(x, y);
+                    let mut neighbors = 0;
+
+                    if self.map.tiles[idx - 1] == TileType::Wall {
+                        neighbors += 1;
+                    }
+                    if self.map.tiles[idx + 1] == TileType::Wall {
+                        neighbors += 1;
+                    }
+                    if self.map.tiles[idx - self.map.width as usize] == TileType::Wall {
+                        neighbors += 1;
+                    }
+                    if self.map.tiles[idx + self.map.width as usize] == TileType::Wall {
+                        neighbors += 1;
+                    }
+                    if self.map.tiles[idx - (self.map.width as usize - 1)] == TileType::Wall {
+                        neighbors += 1;
+                    }
+                    if self.map.tiles[idx - (self.map.width as usize + 1)] == TileType::Wall {
+                        neighbors += 1;
+                    }
+                    if self.map.tiles[idx + (self.map.width as usize - 1)] == TileType::Wall {
+                        neighbors += 1;
+                    }
+                    if self.map.tiles[idx + (self.map.width as usize + 1)] == TileType::Wall {
+                        neighbors += 1;
+                    }
+
+                    if neighbors > 4 || neighbors == 0 {
+                        new_tiles[idx] = TileType::Wall;
+                    } else {
+                        new_tiles[idx] = TileType::Floor;
+                    }
+                }
+            }
+
+            self.map.tiles = new_tiles;
+
+            self.take_snapshot();
+        }
+
+        // Find a starting point; start at the middle and walk left until we find an open tile
+        self.starting_position = Position {
+            x: self.map.width / 2,
+            y: self.map.height / 2,
+        };
+
+        let mut start_idx = self
+            .map
+            .xy_idx(self.starting_position.x, self.starting_position.y);
+
+        while self.map.tiles[start_idx] != TileType::Floor {
+            self.starting_position.x -= 1;
+
+            start_idx = self
+                .map
+                .xy_idx(self.starting_position.x, self.starting_position.y);
+        }
+
+        // Find all tiles we can reach from the starting point
+        let map_starts = vec![start_idx];
+        let dijkstra_map = rltk::DijkstraMap::new(
+            self.map.width,
+            self.map.height,
+            &map_starts,
+            &self.map,
+            200.0,
+        );
+        let mut exit_tile = (0, 0.0f32);
+
+        for (i, tile) in self.map.tiles.iter_mut().enumerate() {
+            if *tile != TileType::Floor {
+                continue;
+            }
+
+            let distance_to_start = dijkstra_map.map[i];
+
+            // TODO(DP): this does not work -- unreachable cells have no f32::MAX value
+            // We can't get to this tile - so we'll make it a wall
+            if distance_to_start == f32::MAX {
+                *tile = TileType::Wall;
+
+                continue;
+            }
+
+            // If it is further away than our current exit candidate, move the exit
+            if distance_to_start > exit_tile.1 {
+                exit_tile.0 = i;
+                exit_tile.1 = distance_to_start;
+            }
+        }
+
+        self.take_snapshot();
+
+        self.map.tiles[exit_tile.0] = TileType::DownStairs;
+
+        self.take_snapshot();
+
+        // Now we build a noise map for use in spawning entities later
+        let mut noise = rltk::FastNoise::seeded(rng.roll_dice(1, 65536) as u64);
+
+        noise.set_noise_type(rltk::NoiseType::Cellular);
+        noise.set_frequency(0.08);
+        noise.set_cellular_distance_function(rltk::CellularDistanceFunction::Manhattan);
+
+        for y in 1..self.map.height - 1 {
+            for x in 1..self.map.width - 1 {
+                let idx = self.map.xy_idx(x, y);
+
+                if self.map.tiles[idx] != TileType::Floor {
+                    continue;
+                }
+
+                let cell_value_f = noise.get_noise(x as f32, y as f32) * 10240.0;
+                let cell_value = cell_value_f as i32;
+
+                if self.noise_areas.contains_key(&cell_value) {
+                    self.noise_areas.get_mut(&cell_value).unwrap().push(idx);
+                } else {
+                    self.noise_areas.insert(cell_value, vec![idx]);
+                }
+            }
+        }
+    }
+}
--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -2,6 +2,7 @@ mod bsp_dungeon;
 mod bsp_interior;
 mod cellular_automata;
 mod common;
+mod drunkard;
 mod simple_map;
 
 use rltk::RandomNumberGenerator;
@@ -12,6 +13,7 @@ use crate::map::Map;
 use crate::map_builders::bsp_dungeon::BspDungeonBuilder;
 use crate::map_builders::bsp_interior::BspInteriorBuilder;
 use crate::map_builders::cellular_automata::CellularAutomataBuilder;
+use crate::map_builders::drunkard::DrunkardsWalkBuilder;
 use crate::map_builders::simple_map::SimpleMapBuilder;
 
 pub trait MapBuilder {
@@ -24,13 +26,16 @@ pub trait MapBuilder {
 }
 
 pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
-    let mut rng = RandomNumberGenerator::new();
-    let builder = rng.roll_dice(1, 4);
+    // TODO(DP): uncomment after tests
+    // let mut rng = RandomNumberGenerator::new();
+    // let builder = rng.roll_dice(1, 4);
+    //
+    // match builder {
+    //     1 => Box::new(BspDungeonBuilder::new(new_depth)),
+    //     2 => Box::new(BspInteriorBuilder::new(new_depth)),
+    //     3 => Box::new(CellularAutomataBuilder::new(new_depth)),
+    //     _ => Box::new(SimpleMapBuilder::new(new_depth)),
+    // }
 
-    match builder {
-        1 => Box::new(BspDungeonBuilder::new(new_depth)),
-        2 => Box::new(BspInteriorBuilder::new(new_depth)),
-        3 => Box::new(CellularAutomataBuilder::new(new_depth)),
-        _ => Box::new(SimpleMapBuilder::new(new_depth)),
-    }
+    Box::new(DrunkardsWalkBuilder::new(new_depth))
 }
