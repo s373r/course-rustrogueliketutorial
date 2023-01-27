ca0d32d313c92a183065d88fdf7fa551f8be7932
Dima Pristupa <dima.pristupa@pm.me>
2023-01-27 18:11:38 +0200

    🚧 4.8. Diffusion-limited aggregation maps - Central Attractor: finish

---
+++ src/map_builders/dla.rs
@@ -0,0 +1,255 @@
+use rltk::RandomNumberGenerator;
+use specs::World;
+use std::collections::HashMap;
+
+use crate::components::Position;
+use crate::map::{Map, TileType};
+use crate::map_builders::common::*;
+use crate::map_builders::MapBuilder;
+use crate::{spawner, SHOW_MAPGEN_VISUALIZER};
+
+#[derive(PartialEq, Copy, Clone)]
+pub enum DLAAlgorithm {
+    WalkInwards,
+    WalkOutwards,
+    CentralAttractor,
+}
+
+#[derive(PartialEq, Copy, Clone)]
+pub enum DLASymmetry {
+    None,
+    Horizontal,
+    Vertical,
+    Both,
+}
+
+pub struct DLABuilder {
+    map: Map,
+    starting_position: Position,
+    depth: i32,
+    history: Vec<Map>,
+    noise_areas: HashMap<i32, Vec<usize>>,
+    algorithm: DLAAlgorithm,
+    brush_size: i32,
+    symmetry: DLASymmetry,
+    floor_percent: f32,
+}
+
+impl DLABuilder {
+    pub fn new(new_depth: i32) -> DLABuilder {
+        DLABuilder {
+            map: Map::new(new_depth),
+            starting_position: Position { x: 0, y: 0 },
+            depth: new_depth,
+            history: Vec::new(),
+            noise_areas: HashMap::new(),
+            algorithm: DLAAlgorithm::WalkInwards,
+            brush_size: 1,
+            symmetry: DLASymmetry::None,
+            floor_percent: 0.25,
+        }
+    }
+
+    fn build(&mut self) {
+        let mut rng = RandomNumberGenerator::new();
+
+        // Carve a starting seed
+        self.starting_position = Position {
+            x: self.map.width / 2,
+            y: self.map.height / 2,
+        };
+
+        let start_idx = self
+            .map
+            .xy_idx(self.starting_position.x, self.starting_position.y);
+
+        self.take_snapshot();
+
+        self.map.tiles[start_idx] = TileType::Floor;
+        self.map.tiles[start_idx - 1] = TileType::Floor;
+        self.map.tiles[start_idx + 1] = TileType::Floor;
+        // TODO(DP): extract to a function -- y - 1 and y + 1
+        self.map.tiles[start_idx - self.map.width as usize] = TileType::Floor;
+        self.map.tiles[start_idx + self.map.width as usize] = TileType::Floor;
+
+        // Random walker
+        let total_tiles = self.map.width * self.map.height;
+        let desired_floor_tiles = (self.floor_percent * total_tiles as f32) as usize;
+        let mut floor_tile_count = self
+            .map
+            .tiles
+            .iter()
+            .filter(|a| **a == TileType::Floor)
+            .count();
+
+        while floor_tile_count < desired_floor_tiles {
+            match self.algorithm {
+                DLAAlgorithm::WalkInwards => {
+                    let mut digger_x = rng.roll_dice(1, self.map.width - 3) + 1;
+                    let mut digger_y = rng.roll_dice(1, self.map.height - 3) + 1;
+                    let mut prev_x = digger_x;
+                    let mut prev_y = digger_y;
+                    let mut digger_idx = self.map.xy_idx(digger_x, digger_y);
+
+                    while self.map.tiles[digger_idx] == TileType::Wall {
+                        prev_x = digger_x;
+                        prev_y = digger_y;
+
+                        let stagger_direction = rng.roll_dice(1, 4);
+
+                        match stagger_direction {
+                            1 => {
+                                if digger_x > 2 {
+                                    digger_x -= 1;
+                                }
+                            }
+                            2 => {
+                                if digger_x < self.map.width - 2 {
+                                    digger_x += 1;
+                                }
+                            }
+                            3 => {
+                                if digger_y > 2 {
+                                    digger_y -= 1;
+                                }
+                            }
+                            _ => {
+                                if digger_y < self.map.height - 2 {
+                                    digger_y += 1;
+                                }
+                            }
+                        }
+
+                        digger_idx = self.map.xy_idx(digger_x, digger_y);
+                    }
+
+                    self.paint(prev_x, prev_y);
+                }
+                DLAAlgorithm::WalkOutwards => {
+                    let mut digger_x = self.starting_position.x;
+                    let mut digger_y = self.starting_position.y;
+                    let mut digger_idx = self.map.xy_idx(digger_x, digger_y);
+
+                    while self.map.tiles[digger_idx] == TileType::Floor {
+                        let stagger_direction = rng.roll_dice(1, 4);
+
+                        match stagger_direction {
+                            1 => {
+                                if digger_x > 2 {
+                                    digger_x -= 1;
+                                }
+                            }
+                            2 => {
+                                if digger_x < self.map.width - 2 {
+                                    digger_x += 1;
+                                }
+                            }
+                            3 => {
+                                if digger_y > 2 {
+                                    digger_y -= 1;
+                                }
+                            }
+                            _ => {
+                                if digger_y < self.map.height - 2 {
+                                    digger_y += 1;
+                                }
+                            }
+                        }
+
+                        digger_idx = self.map.xy_idx(digger_x, digger_y);
+                    }
+
+                    self.paint(digger_x, digger_y);
+                }
+                DLAAlgorithm::CentralAttractor => {
+                    let mut digger_x = rng.roll_dice(1, self.map.width - 3) + 1;
+                    let mut digger_y = rng.roll_dice(1, self.map.height - 3) + 1;
+                    let mut prev_x = digger_x;
+                    let mut prev_y = digger_y;
+                    let mut digger_idx = self.map.xy_idx(digger_x, digger_y);
+
+                    let mut path = rltk::line2d(
+                        rltk::LineAlg::Bresenham,
+                        rltk::Point::new(digger_x, digger_y),
+                        rltk::Point::new(self.starting_position.x, self.starting_position.y),
+                    );
+
+                    while self.map.tiles[digger_idx] == TileType::Wall && !path.is_empty() {
+                        prev_x = digger_x;
+                        prev_y = digger_y;
+                        digger_x = path[0].x;
+                        digger_y = path[0].y;
+                        path.remove(0);
+                        digger_idx = self.map.xy_idx(digger_x, digger_y);
+                    }
+
+                    self.paint(prev_x, prev_y);
+                }
+            }
+
+            self.take_snapshot();
+
+            floor_tile_count = self
+                .map
+                .tiles
+                .iter()
+                .filter(|a| **a == TileType::Floor)
+                .count();
+        }
+
+        // Find all tiles we can reach from the starting point
+        let exit_tile = remove_unreachable_areas_returning_most_distant(&mut self.map, start_idx);
+
+        self.take_snapshot();
+
+        // Place the stairs
+        self.map.tiles[exit_tile] = TileType::DownStairs;
+
+        self.take_snapshot();
+
+        // Now we build a noise map for use in spawning entities later
+        self.noise_areas = generate_voronoi_spawn_regions(&self.map, &mut rng);
+    }
+
+    fn paint(&mut self, x: i32, y: i32) {
+        let digger_idx = self.map.xy_idx(x, y);
+
+        self.map.tiles[digger_idx] = TileType::Floor;
+    }
+}
+
+impl MapBuilder for DLABuilder {
+    fn build_map(&mut self) {
+        self.build();
+    }
+
+    fn spawn_entities(&self, ecs: &mut World) {
+        for area in self.noise_areas.iter() {
+            spawner::spawn_region(ecs, area.1, self.depth);
+        }
+    }
+
+    fn get_map(&self) -> Map {
+        self.map.clone()
+    }
+
+    fn get_starting_position(&self) -> Position {
+        self.starting_position.clone()
+    }
+
+    fn get_snapshot_history(&self) -> Vec<Map> {
+        self.history.clone()
+    }
+
+    fn take_snapshot(&mut self) {
+        if !SHOW_MAPGEN_VISUALIZER {
+            return;
+        }
+
+        let mut snapshot = self.map.clone();
+
+        snapshot.revealed_tiles.fill(true);
+
+        self.history.push(snapshot);
+    }
+}
--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -2,6 +2,7 @@ mod bsp_dungeon;
 mod bsp_interior;
 mod cellular_automata;
 mod common;
+mod dla;
 mod drunkard;
 mod maze;
 mod simple_map;
@@ -14,6 +15,7 @@ use crate::map::Map;
 use crate::map_builders::bsp_dungeon::BspDungeonBuilder;
 use crate::map_builders::bsp_interior::BspInteriorBuilder;
 use crate::map_builders::cellular_automata::CellularAutomataBuilder;
+use crate::map_builders::dla::DLABuilder;
 use crate::map_builders::drunkard::DrunkardsWalkBuilder;
 use crate::map_builders::maze::MazeBuilder;
 use crate::map_builders::simple_map::SimpleMapBuilder;
@@ -28,17 +30,21 @@ pub trait MapBuilder {
 }
 
 pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
-    let mut rng = RandomNumberGenerator::new();
-    let builder = rng.roll_dice(1, 8);
+    // TODO(DP): uncomment after
+    // let mut rng = RandomNumberGenerator::new();
+    // let builder = rng.roll_dice(1, 9);
+    //
+    // match builder {
+    //     1 => Box::new(BspDungeonBuilder::new(new_depth)),
+    //     2 => Box::new(BspInteriorBuilder::new(new_depth)),
+    //     3 => Box::new(CellularAutomataBuilder::new(new_depth)),
+    //     4 => Box::new(DrunkardsWalkBuilder::open_area(new_depth)),
+    //     5 => Box::new(DrunkardsWalkBuilder::open_halls(new_depth)),
+    //     6 => Box::new(DrunkardsWalkBuilder::winding_passages(new_depth)),
+    //     7 => Box::new(MazeBuilder::new(new_depth)),
+    //     8 => Box::new(DLABuilder::new(new_depth)),
+    //     _ => Box::new(SimpleMapBuilder::new(new_depth)),
+    // }
 
-    match builder {
-        1 => Box::new(BspDungeonBuilder::new(new_depth)),
-        2 => Box::new(BspInteriorBuilder::new(new_depth)),
-        3 => Box::new(CellularAutomataBuilder::new(new_depth)),
-        4 => Box::new(DrunkardsWalkBuilder::open_area(new_depth)),
-        5 => Box::new(DrunkardsWalkBuilder::open_halls(new_depth)),
-        6 => Box::new(DrunkardsWalkBuilder::winding_passages(new_depth)),
-        7 => Box::new(MazeBuilder::new(new_depth)),
-        _ => Box::new(SimpleMapBuilder::new(new_depth)),
-    }
+    Box::new(DLABuilder::new(new_depth))
 }
