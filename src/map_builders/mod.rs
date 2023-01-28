28bb719bf671456ffa1dc59cfce8a6d0bfb1c8f0
Dima Pristupa <dima.pristupa@pm.me>
2023-01-28 14:04:53 +0200

    ✨ 4.8. Diffusion-limited aggregation maps: finish

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -30,21 +30,21 @@ pub trait MapBuilder {
 }
 
 pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
-    // TODO(DP): uncomment after
-    // let mut rng = RandomNumberGenerator::new();
-    // let builder = rng.roll_dice(1, 9);
-    //
-    // match builder {
-    //     1 => Box::new(BspDungeonBuilder::new(new_depth)),
-    //     2 => Box::new(BspInteriorBuilder::new(new_depth)),
-    //     3 => Box::new(CellularAutomataBuilder::new(new_depth)),
-    //     4 => Box::new(DrunkardsWalkBuilder::open_area(new_depth)),
-    //     5 => Box::new(DrunkardsWalkBuilder::open_halls(new_depth)),
-    //     6 => Box::new(DrunkardsWalkBuilder::winding_passages(new_depth)),
-    //     7 => Box::new(MazeBuilder::new(new_depth)),
-    //     8 => Box::new(DLABuilder::new(new_depth)),
-    //     _ => Box::new(SimpleMapBuilder::new(new_depth)),
-    // }
+    let mut rng = RandomNumberGenerator::new();
+    let builder = rng.roll_dice(1, 12);
 
-    Box::new(DLABuilder::new(new_depth))
+    match builder {
+        1 => Box::new(BspDungeonBuilder::new(new_depth)),
+        2 => Box::new(BspInteriorBuilder::new(new_depth)),
+        3 => Box::new(CellularAutomataBuilder::new(new_depth)),
+        4 => Box::new(DrunkardsWalkBuilder::open_area(new_depth)),
+        5 => Box::new(DrunkardsWalkBuilder::open_halls(new_depth)),
+        6 => Box::new(DrunkardsWalkBuilder::winding_passages(new_depth)),
+        7 => Box::new(MazeBuilder::new(new_depth)),
+        8 => Box::new(DLABuilder::walk_inwards(new_depth)),
+        9 => Box::new(DLABuilder::walk_outwards(new_depth)),
+        10 => Box::new(DLABuilder::central_attractor(new_depth)),
+        11 => Box::new(DLABuilder::insectoid(new_depth)),
+        _ => Box::new(SimpleMapBuilder::new(new_depth)),
+    }
 }
