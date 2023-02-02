421adf3b56ec57599712525a42fb7f4b8a3233f9
Dima Pristupa <dima.pristupa@pm.me>
2023-02-02 22:32:29 +0200

    🚧 4.14. Layering/Builder Chaining - Modifying SimpleMapBuilder: finish

--- src/map_builders/simple_map.rs
+++ src/map_builders/simple_map.rs
@@ -3,46 +3,43 @@ use rltk::RandomNumberGenerator;
 use crate::components::Position;
 use crate::map::{Map, TileType};
 use crate::map_builders::common::*;
-use crate::map_builders::MapBuilder;
+use crate::map_builders::{BuilderMap, InitialMapBuilder, MapBuilder};
 use crate::rect::Rect;
 use crate::{spawner, SHOW_MAPGEN_VISUALIZER};
 
-pub struct SimpleMapBuilder {
-    map: Map,
-    starting_position: Position,
-    depth: i32,
-    rooms: Vec<Rect>,
-    history: Vec<Map>,
-    spawn_list: Vec<SpawnEntity>,
+pub struct SimpleMapBuilder {}
+
+impl InitialMapBuilder for SimpleMapBuilder {
+    #[allow(dead_code)]
+    fn build_map(&mut self, rng: &mut RandomNumberGenerator, build_data: &mut BuilderMap) {
+        self.rooms_and_corridors(rng, build_data);
+    }
 }
 
 impl SimpleMapBuilder {
-    pub fn new(new_depth: i32) -> SimpleMapBuilder {
-        SimpleMapBuilder {
-            map: Map::new(new_depth),
-            starting_position: Position { x: 0, y: 0 },
-            depth: new_depth,
-            rooms: Vec::new(),
-            history: Vec::new(),
-            spawn_list: Vec::new(),
-        }
+    #[allow(dead_code)]
+    pub fn new() -> Box<SimpleMapBuilder> {
+        Box::new(SimpleMapBuilder {})
     }
 
-    pub fn rooms_and_corridors(&mut self) {
+    pub fn rooms_and_corridors(
+        &mut self,
+        rng: &mut RandomNumberGenerator,
+        build_data: &mut BuilderMap,
+    ) {
         const MAX_ROOMS: i32 = 30;
         const MIN_SIZE: i32 = 6;
         const MAX_SIZE: i32 = 10;
 
-        let mut rng = RandomNumberGenerator::new();
+        let mut rooms: Vec<Rect> = Vec::new();
 
         for _ in 0..MAX_ROOMS {
             let w = rng.range(MIN_SIZE, MAX_SIZE);
             let h = rng.range(MIN_SIZE, MAX_SIZE);
-            let x = rng.roll_dice(1, self.map.width - w - 1) - 1;
-            let y = rng.roll_dice(1, self.map.height - h - 1) - 1;
+            let x = rng.roll_dice(1, build_data.map.width - w - 1) - 1;
+            let y = rng.roll_dice(1, build_data.map.height - h - 1) - 1;
             let new_room = Rect::new(x, y, w, h);
-            let has_intersect_other_rooms = self
-                .rooms
+            let has_intersect_other_rooms = rooms
                 .iter()
                 .any(|another_room| new_room.intersect(another_room));
 
@@ -50,42 +47,29 @@ impl SimpleMapBuilder {
                 continue;
             }
 
-            apply_room(&mut self.map, &new_room);
+            apply_room(&mut build_data.map, &new_room);
 
             self.take_snapshot();
 
-            if !self.rooms.is_empty() {
+            if !rooms.is_empty() {
                 let (new_x, new_y) = new_room.center();
-                let (prev_x, prev_y) = self.rooms.last().unwrap().center();
+                let (prev_x, prev_y) = rooms.last().unwrap().center();
 
                 if rng.range(0, 2) == 1 {
-                    apply_horizontal_tunnel(&mut self.map, prev_x, new_x, prev_y);
-                    apply_vertical_tunnel(&mut self.map, prev_y, new_y, new_x);
+                    apply_horizontal_tunnel(&mut build_data.map, prev_x, new_x, prev_y);
+                    apply_vertical_tunnel(&mut build_data.map, prev_y, new_y, new_x);
                 } else {
-                    apply_vertical_tunnel(&mut self.map, prev_y, new_y, prev_x);
-                    apply_horizontal_tunnel(&mut self.map, prev_x, new_x, new_y);
+                    apply_vertical_tunnel(&mut build_data.map, prev_y, new_y, prev_x);
+                    apply_horizontal_tunnel(&mut build_data.map, prev_x, new_x, new_y);
                 }
             }
 
-            self.rooms.push(new_room);
+            rooms.push(new_room);
 
             self.take_snapshot();
         }
 
-        let stairs_position = self.rooms.last().unwrap().center();
-        let stairs_idx = self.map.xy_idx(stairs_position.0, stairs_position.1);
-
-        self.map.tiles[stairs_idx] = TileType::DownStairs;
-
-        // Start position
-        let (x, y) = self.rooms.first().unwrap().center();
-
-        self.starting_position = Position { x, y };
-
-        // Spawn some entities
-        for room in self.rooms.iter().skip(1) {
-            spawner::spawn_room(&self.map, &mut rng, room, self.depth, &mut self.spawn_list);
-        }
+        build_data.rooms = Some(rooms);
     }
 }
 
