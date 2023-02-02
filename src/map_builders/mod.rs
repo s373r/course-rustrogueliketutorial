aebf525869f6033b9b75388715e4810473b7d3c7
Dima Pristupa <dima.pristupa@pm.me>
2023-02-02 16:03:37 +0200

    ✨ 4.13. Room Vaults: finish

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -44,44 +44,45 @@ pub trait MapBuilder {
 }
 
 pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
-    Box::new(PrefabBuilder::vaults(
-        new_depth,
-        Box::new(PrefabBuilder::sectional(
+    let mut rng = RandomNumberGenerator::new();
+    let builder = rng.roll_dice(1, 17);
+
+    let mut result: Box<dyn MapBuilder> = match builder {
+        1 => Box::new(BspDungeonBuilder::new(new_depth)),
+        2 => Box::new(BspInteriorBuilder::new(new_depth)),
+        3 => Box::new(CellularAutomataBuilder::new(new_depth)),
+        4 => Box::new(DrunkardsWalkBuilder::open_area(new_depth)),
+        5 => Box::new(DrunkardsWalkBuilder::open_halls(new_depth)),
+        6 => Box::new(DrunkardsWalkBuilder::winding_passages(new_depth)),
+        7 => Box::new(DrunkardsWalkBuilder::fat_passages(new_depth)),
+        8 => Box::new(DrunkardsWalkBuilder::fearful_symmetry(new_depth)),
+        9 => Box::new(MazeBuilder::new(new_depth)),
+        10 => Box::new(DLABuilder::walk_inwards(new_depth)),
+        11 => Box::new(DLABuilder::walk_outwards(new_depth)),
+        12 => Box::new(DLABuilder::central_attractor(new_depth)),
+        13 => Box::new(DLABuilder::insectoid(new_depth)),
+        14 => Box::new(VoronoiCellBuilder::pythagoras(new_depth)),
+        15 => Box::new(VoronoiCellBuilder::manhattan(new_depth)),
+        16 => Box::new(PrefabBuilder::constant(
+            new_depth,
+            prefab_builder::prefab_levels::WFC_POPULATED,
+        )),
+        _ => Box::new(SimpleMapBuilder::new(new_depth)),
+    };
+
+    if rng.roll_dice(1, 3) == 1 {
+        result = Box::new(WaveformCollapseBuilder::derived_map(new_depth, result));
+    }
+
+    if rng.roll_dice(1, 20) == 1 {
+        result = Box::new(PrefabBuilder::sectional(
             new_depth,
             prefab_builder::prefab_sections::UNDERGROUND_FORT,
-            Box::new(WaveformCollapseBuilder::derived_map(
-                new_depth,
-                Box::new(CellularAutomataBuilder::new(new_depth)),
-            )),
-        )),
-    ))
+            result,
+        ));
+    }
 
-    // TODO(DP): return after tests
-    // let mut rng = RandomNumberGenerator::new();
-    // let builder = rng.roll_dice(1, 17);
-    //
-    // let mut result: Box<dyn MapBuilder> = match builder {
-    //     1 => Box::new(BspDungeonBuilder::new(new_depth)),
-    //     2 => Box::new(BspInteriorBuilder::new(new_depth)),
-    //     3 => Box::new(CellularAutomataBuilder::new(new_depth)),
-    //     4 => Box::new(DrunkardsWalkBuilder::open_area(new_depth)),
-    //     5 => Box::new(DrunkardsWalkBuilder::open_halls(new_depth)),
-    //     6 => Box::new(DrunkardsWalkBuilder::winding_passages(new_depth)),
-    //     7 => Box::new(DrunkardsWalkBuilder::fat_passages(new_depth)),
-    //     8 => Box::new(DrunkardsWalkBuilder::fearful_symmetry(new_depth)),
-    //     9 => Box::new(MazeBuilder::new(new_depth)),
-    //     10 => Box::new(DLABuilder::walk_inwards(new_depth)),
-    //     11 => Box::new(DLABuilder::walk_outwards(new_depth)),
-    //     12 => Box::new(DLABuilder::central_attractor(new_depth)),
-    //     13 => Box::new(DLABuilder::insectoid(new_depth)),
-    //     14 => Box::new(VoronoiCellBuilder::pythagoras(new_depth)),
-    //     15 => Box::new(VoronoiCellBuilder::manhattan(new_depth)),
-    //     _ => Box::new(SimpleMapBuilder::new(new_depth)),
-    // };
-    //
-    // if rng.roll_dice(1, 3) == 1 {
-    //     result = Box::new(WaveformCollapseBuilder::derived_map(new_depth, result));
-    // }
-    //
-    // result
+    result = Box::new(PrefabBuilder::vaults(new_depth, result));
+
+    result
 }
