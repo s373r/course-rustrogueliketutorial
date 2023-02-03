b34841ee8ee977d9d8465316d0a3d7e4cacd2af7
Dima Pristupa <dima.pristupa@pm.me>
2023-02-03 21:15:46 +0200

    🚧 4.14. Layering/Builder Chaining - Updating Voronoi Maps: finish

--- src/map_builders/voronoi.rs
+++ src/map_builders/voronoi.rs
@@ -1,11 +1,6 @@
-use rltk::RandomNumberGenerator;
-use std::collections::HashMap;
-
-use crate::components::Position;
-use crate::map::{Map, TileType};
-use crate::map_builders::common::*;
-use crate::map_builders::MapBuilder;
-use crate::{spawner, SHOW_MAPGEN_VISUALIZER};
+use crate::map::TileType;
+use crate::map_builders::BuilderMap;
+use crate::map_builders::InitialMapBuilder;
 
 #[derive(PartialEq, Copy, Clone)]
 #[allow(dead_code)]
@@ -16,84 +11,46 @@ pub enum DistanceAlgorithm {
 }
 
 pub struct VoronoiCellBuilder {
-    map: Map,
-    starting_position: Position,
-    depth: i32,
-    history: Vec<Map>,
-    noise_areas: HashMap<i32, Vec<usize>>,
     n_seeds: usize,
     distance_algorithm: DistanceAlgorithm,
-    spawn_list: Vec<SpawnEntity>,
 }
 
-impl MapBuilder for VoronoiCellBuilder {
-    fn build_map(&mut self) {
-        self.build();
-    }
-
-    fn get_map(&self) -> Map {
-        self.map.clone()
-    }
-
-    fn get_starting_position(&self) -> Position {
-        self.starting_position.clone()
-    }
-
-    fn get_snapshot_history(&self) -> Vec<Map> {
-        self.history.clone()
-    }
-
-    fn take_snapshot(&mut self) {
-        if !SHOW_MAPGEN_VISUALIZER {
-            return;
-        }
-
-        let mut snapshot = self.map.clone();
-
-        snapshot.revealed_tiles.fill(true);
-
-        self.history.push(snapshot);
-    }
-
-    fn get_spawn_list(&self) -> &Vec<SpawnEntity> {
-        &self.spawn_list
+impl InitialMapBuilder for VoronoiCellBuilder {
+    #[allow(dead_code)]
+    fn build_map(&mut self, rng: &mut rltk::RandomNumberGenerator, build_data: &mut BuilderMap) {
+        self.build(rng, build_data);
     }
 }
 
 impl VoronoiCellBuilder {
     #[allow(dead_code)]
-    fn new(new_depth: i32, distance_algorithm: DistanceAlgorithm) -> VoronoiCellBuilder {
-        VoronoiCellBuilder {
-            map: Map::new(new_depth),
-            starting_position: Position { x: 0, y: 0 },
-            depth: new_depth,
-            history: Vec::new(),
-            noise_areas: HashMap::new(),
+    fn new(distance_algorithm: DistanceAlgorithm) -> Box<VoronoiCellBuilder> {
+        Box::new(VoronoiCellBuilder {
             n_seeds: 64,
             distance_algorithm,
-            spawn_list: Vec::new(),
-        }
+        })
     }
 
-    pub fn pythagoras(new_depth: i32) -> VoronoiCellBuilder {
-        Self::new(new_depth, DistanceAlgorithm::Pythagoras)
+    pub fn pythagoras() -> Box<VoronoiCellBuilder> {
+        Self::new(DistanceAlgorithm::Pythagoras)
     }
 
-    pub fn manhattan(new_depth: i32) -> VoronoiCellBuilder {
-        Self::new(new_depth, DistanceAlgorithm::Manhattan)
+    pub fn manhattan() -> Box<VoronoiCellBuilder> {
+        Self::new(DistanceAlgorithm::Manhattan)
     }
 
     #[allow(clippy::map_entry)]
-    fn build(&mut self) {
-        let mut rng = RandomNumberGenerator::new();
+    fn build(&mut self, rng: &mut rltk::RandomNumberGenerator, build_data: &mut BuilderMap) {
+        let map_width = build_data.map.width;
+        let map_height = build_data.map.height;
 
         // Make a Voronoi diagram. We'll do this the hard way to learn about the technique!
         let mut voronoi_seeds: Vec<(usize, rltk::Point)> = Vec::new();
 
         while voronoi_seeds.len() < self.n_seeds {
-            let vx = rng.roll_dice(1, self.map.width - 1);
-            let vy = rng.roll_dice(1, self.map.height - 1);
-            let vidx = self.map.xy_idx(vx, vy);
+            let vx = rng.roll_dice(1, map_width - 1);
+            let vy = rng.roll_dice(1, map_height - 1);
+            let vidx = build_data.map.xy_idx(vx, vy);
             let candidate = (vidx, rltk::Point::new(vx, vy));
 
             if !voronoi_seeds.contains(&candidate) {
@@ -102,12 +59,11 @@ impl VoronoiCellBuilder {
         }
 
         let mut voronoi_distance = vec![(0, 0.0f32); self.n_seeds];
-        let mut voronoi_membership: Vec<i32> =
-            vec![0; self.map.width as usize * self.map.height as usize];
+        let mut voronoi_membership: Vec<i32> = vec![0; map_width as usize * map_height as usize];
 
         for (i, vid) in voronoi_membership.iter_mut().enumerate() {
-            let x = i as i32 % self.map.width;
-            let y = i as i32 / self.map.width;
+            let x = i as i32 % map_width;
+            let y = i as i32 / map_width;
 
             for (seed, pos) in voronoi_seeds.iter().enumerate() {
                 let distance = match self.distance_algorithm {
@@ -129,72 +85,30 @@ impl VoronoiCellBuilder {
             *vid = voronoi_distance[0].0 as i32;
         }
 
-        for y in 1..self.map.height - 1 {
-            for x in 1..self.map.width - 1 {
+        for y in 1..map_height - 1 {
+            for x in 1..map_width - 1 {
                 let mut neighbors = 0;
-                let my_idx = self.map.xy_idx(x, y);
+                let my_idx = build_data.map.xy_idx(x, y);
                 let my_seed = voronoi_membership[my_idx];
 
-                if voronoi_membership[self.map.xy_idx(x - 1, y)] != my_seed {
+                if voronoi_membership[build_data.map.xy_idx(x - 1, y)] != my_seed {
                     neighbors += 1;
                 }
-                if voronoi_membership[self.map.xy_idx(x + 1, y)] != my_seed {
+                if voronoi_membership[build_data.map.xy_idx(x + 1, y)] != my_seed {
                     neighbors += 1;
                 }
-                if voronoi_membership[self.map.xy_idx(x, y - 1)] != my_seed {
+                if voronoi_membership[build_data.map.xy_idx(x, y - 1)] != my_seed {
                     neighbors += 1;
                 }
-                if voronoi_membership[self.map.xy_idx(x, y + 1)] != my_seed {
+                if voronoi_membership[build_data.map.xy_idx(x, y + 1)] != my_seed {
                     neighbors += 1;
                 }
 
                 if neighbors < 2 {
-                    self.map.tiles[my_idx] = TileType::Floor;
+                    build_data.map.tiles[my_idx] = TileType::Floor;
                 }
             }
-            self.take_snapshot();
-        }
-
-        // Find a starting point; start at the middle and walk left until we find an open tile
-        self.starting_position = Position {
-            x: self.map.width / 2,
-            y: self.map.height / 2,
-        };
-
-        let mut start_idx = self
-            .map
-            .xy_idx(self.starting_position.x, self.starting_position.y);
-
-        while self.map.tiles[start_idx] != TileType::Floor {
-            self.starting_position.x -= 1;
-            start_idx = self
-                .map
-                .xy_idx(self.starting_position.x, self.starting_position.y);
-        }
-        self.take_snapshot();
-
-        // Find all tiles we can reach from the starting point
-        let exit_tile = remove_unreachable_areas_returning_most_distant(&mut self.map, start_idx);
-
-        self.take_snapshot();
-
-        // Place the stairs
-        self.map.tiles[exit_tile] = TileType::DownStairs;
-
-        self.take_snapshot();
-
-        // Now we build a noise map for use in spawning entities later
-        self.noise_areas = generate_voronoi_spawn_regions(&self.map, &mut rng);
-
-        // Spawn the entities
-        for area in self.noise_areas.iter() {
-            spawner::spawn_region(
-                &self.map,
-                &mut rng,
-                area.1,
-                self.depth,
-                &mut self.spawn_list,
-            );
+            build_data.take_snapshot();
         }
     }
 }
