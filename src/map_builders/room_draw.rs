57547d467b9297207441992d1ca99ab32576cbd9
Dima Pristupa <dima.pristupa@pm.me>
2023-02-06 20:24:16 +0200

    🚧 4.16. Room Builders - Circular Rooms: finish

--- src/map_builders/room_draw.rs
+++ src/map_builders/room_draw.rs
@@ -1,5 +1,6 @@
 use crate::map::TileType;
 use crate::map_builders::{BuilderMap, MetaMapBuilder};
+use crate::rect::Rect;
 
 pub struct RoomDrawer {}
 
@@ -22,15 +23,38 @@ impl RoomDrawer {
         let rooms = rooms_builder.clone();
 
         for room in rooms.iter() {
-            for y in room.y1 + 1..=room.y2 {
-                for x in room.x1 + 1..=room.x2 {
-                    let idx = build_data.map.xy_idx(x, y);
-                    if build_data.map.is_valid_idx(idx) {
-                        build_data.map.tiles[idx] = TileType::Floor;
-                    }
+            self.circle(build_data, room);
+            build_data.take_snapshot();
+        }
+    }
+
+    fn rectangle(&mut self, build_data: &mut BuilderMap, room: &Rect) {
+        for y in room.y1 + 1..=room.y2 {
+            for x in room.x1 + 1..=room.x2 {
+                let idx = build_data.map.xy_idx(x, y);
+                if build_data.map.is_valid_idx(idx) {
+                    build_data.map.tiles[idx] = TileType::Floor;
                 }
             }
-            build_data.take_snapshot();
+        }
+    }
+
+    fn circle(&mut self, build_data: &mut BuilderMap, room: &Rect) {
+        let radius = i32::min(room.x2 - room.x1, room.y2 - room.y1) as f32 / 2.0;
+        let room_center = {
+            let (x, y) = room.center();
+            rltk::Point::new(x, y)
+        };
+        for y in room.y1..=room.y2 {
+            for x in room.x1..=room.x2 {
+                let room_point = rltk::Point::new(x, y);
+                let distance = rltk::DistanceAlg::Pythagoras.distance2d(room_center, room_point);
+                let idx = build_data.map.xy_idx(x, y);
+
+                if build_data.map.is_valid_idx(idx) && distance <= radius {
+                    build_data.map.tiles[idx] = TileType::Floor;
+                }
+            }
         }
     }
 }
